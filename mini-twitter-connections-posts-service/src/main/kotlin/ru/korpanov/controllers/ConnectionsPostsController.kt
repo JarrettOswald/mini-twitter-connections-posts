a37d65f9b397239f8a62a40c0eddf429341ee311
JarrettOswald <go14in83hell@gmail.com>
2023-07-01 08:18:18 +0400

    global change to event

--- mini-twitter-connections-posts-service/src/main/kotlin/ru/korpanov/controllers/ConnectionsPostsController.kt
+++ mini-twitter-connections-posts-service/src/main/kotlin/ru/korpanov/controllers/ConnectionsPostsController.kt
@@ -1,30 +1,32 @@
 package ru.korpanov.controllers
 
+import org.openapitools.api.ConnectionsApi
 import org.openapitools.api.ConnectionsPostsApi
+import org.openapitools.api.PostsApi
+import org.openapitools.api.UsersApi
+import org.openapitools.model.Connection
 import org.openapitools.model.Post
 import org.openapitools.model.PostSummary
-import org.springframework.http.HttpStatus
+import org.openapitools.model.User
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import ru.korpanov.clients.connection.ConnectionClient
-import ru.korpanov.clients.post.PostClient
-import java.util.*
 
 @Controller
 class ConnectionsPostsController(
-    private val postClient: PostClient,
-    private val connectionClient: ConnectionClient
-) : ConnectionsPostsApi {
-
+) : ConnectionsPostsApi, ConnectionsApi, PostsApi, UsersApi {
     override fun getConnectionsPost(username: String): ResponseEntity<List<PostSummary>> {
-        val posts = mutableListOf<Post>()
-        val connections = connectionClient.getFollowerConnectionByUserName(UUID.fromString(username)).body!!
-        connections
-            .filter { it.follower == UUID.fromString(username) }
-            .map { postClient.findPosts(it.followed).body!! }
-            .forEach { posts.addAll(it) }
+        return super.getConnectionsPost(username)
+    }
 
-        val out = posts.map { PostSummary(it.userId, it.title, it.date, it.body) }
-        return ResponseEntity(out, HttpStatus.OK)
+    override fun saveConnection(connection: Connection): ResponseEntity<String> {
+        return super.saveConnection(connection)
+    }
+
+    override fun savePost(post: Post): ResponseEntity<String> {
+        return super.savePost(post)
+    }
+
+    override fun saveUser(user: User): ResponseEntity<String> {
+        return super.saveUser(user)
     }
 }
